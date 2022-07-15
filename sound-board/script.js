44c158adf911e4c63cf3b7bfb4879244265baab8
Mateus <creztfallen@M.S.M>
2022-07-15 12:50:08 -0300

    #day3

---
+++ sound-board/script.js
@@ -0,0 +1,25 @@
+const sounds = ['kick', 'snare', 'hi-hat', 'open-hi-hat', 'floor-tom'];
+
+sounds.forEach((sound) => {
+  const btn = document.createElement('button');
+  btn.classList.add('btn');
+
+  btn.innerText = sound;
+
+  btn.addEventListener('click', () => {
+    stopSongs();
+
+    document.getElementById(sound).play();
+  });
+
+  document.getElementById('buttons').appendChild(btn);
+});
+
+const stopSongs = () => {
+  sounds.forEach((sound) => {
+    const song = document.getElementById(sound);
+
+    song.pause();
+    song.currentTime = 0;
+  });
+};
